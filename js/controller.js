11fab981315b1123bd2060c0495a94e7834e5fca
khummel <hummka01@luther.edu>
2018-03-14 21:06:31 -0500

    json class working

--- js/controller.js
+++ js/controller.js
@@ -1,25 +1,25 @@
 
-class ShoppingController {
+class Controller {
 	constructor(pubSub) {
 		this.pubSub = pubSub;
 		this.shoppingList = new ShoppingList();
 	}
 
 	newItem(itemSpec) {
-		let count = 0;
-		for (var key in itemSpec) {
-			if (itemSpec[key] != "") {
-				count += 1;
-			}
-		}
-		if (count == 6) {
+		// let count = 0;
+		// for (var key in itemSpec) {
+			// if (itemSpec[key] != "") {
+				// count += 1;
+			// }
+		// }
+		// if (count == 6) {
 			let item = new ShoppingItem(itemSpec["name"], itemSpec["section"], itemSpec["quantity"], itemSpec["store"], itemSpec["priority"], itemSpec["price"]);
 			this.shoppingList.addShoppingItem(item);
 			this.pubSub.publish("newitem", this.shoppingList);
-		}
-		else {
+		// }
+		// else {
 			document.getElementById("inputForm").reset();
-		}
+		// }
 	}
 
 	deleteItem(rowId) {
