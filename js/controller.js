21d5fa5190dc8a4f029ee22ad2e0edd6bbaca5ad
khummel <hummka01@luther.edu>
2018-03-14 21:27:46 -0500

    fixing merge

--- js/controller.js
+++ js/controller.js
@@ -14,12 +14,6 @@ class Controller {
 			// }
 		// }
 		// if (count == 6) {
-<<<<<<< HEAD:js/controller.js
-			let item = new ShoppingItem(itemSpec["name"], itemSpec["section"], itemSpec["quantity"], 
-				itemSpec["store"], itemSpec["priority"], itemSpec["price"]);
-=======
-			let item = new ShoppingItem(itemSpec["name"], itemSpec["section"], itemSpec["quantity"], itemSpec["store"], itemSpec["priority"], itemSpec["price"]);
->>>>>>> jsonclass:js/controller.js
 			this.shoppingList.addShoppingItem(item);
 			this.pubSub.publish("newitem", this.shoppingList);
 		// }
